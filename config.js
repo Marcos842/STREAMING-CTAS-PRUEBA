db1f3731585ae89e8b3b71c0fcbb1e1f97b77ad8
Marcos842 <78865108+Marcos842@users.noreply.github.com>
2025-12-31 00:12:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const CONFIG = {
     SUPABASE_URL: 'https://lvezpnddsgdponvqkkdz.supabase.co',
     SUPABASE_KEY: 'sb_publishable_vlhXBmTnvfPLJ-L_HYfDJw_ihwaKQ1C',
     ADMIN_USER: 'admin192i3i904i30djqodaodjaokjd23993d993d93dk',
-    ADMIN_PASSWORD: 'password20i493i9d3jdj19s1d0dj30d3jd3w'
+    ADMIN_PASS: 'password20i493i9d3jdj19s1d0dj30d3jd3w'
 };
